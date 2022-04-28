9b6c1dd0fabdc1eb80f291996fc8c89b9fca091e
guilherme-kunz <guilhermebutzenkunz@gmail.com>
2022-04-28 11:22:05 -0300

    First Commit

---
+++ app/src/main/java/guilhermekunz/com/br/geolocationwebviewapp/MainActivity.kt
@@ -0,0 +1,114 @@
+package guilhermekunz.com.br.geolocationwebviewapp
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.webkit.GeolocationPermissions
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import guilhermekunz.com.br.geolocationwebviewapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    var mGeoLocationRequestOrigin: String? = null
+    var mGeoLocationCallback: GeolocationPermissions.Callback? = null
+    val MY_PERMISSIONS_REQUEST_LOCATION = 1001
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        webView()
+    }
+
+    private fun webView() {
+        val myWebView: WebView = binding.webView
+        myWebView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView,
+                url: String
+            ): Boolean {
+                view.loadUrl(url)
+                return true
+            }
+        }
+        binding.webView.webChromeClient = object : WebChromeClient() {
+            override fun onGeolocationPermissionsShowPrompt(
+                origin: String?,
+                callback: GeolocationPermissions.Callback?
+            ) {
+                super.onGeolocationPermissionsShowPrompt(origin, callback)
+                mGeoLocationRequestOrigin = null
+                mGeoLocationCallback = null
+                if (ContextCompat.checkSelfPermission(
+                        this@MainActivity,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(
+                            this@MainActivity,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        )
+                    ) {
+                        handlerUserLocationPermissionDenial()
+                        mGeoLocationRequestOrigin = origin
+                        mGeoLocationCallback = callback
+                        ActivityCompat.requestPermissions(
+                            this@MainActivity,
+                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                            MY_PERMISSIONS_REQUEST_LOCATION
+                        )
+                    } else {
+                        mGeoLocationRequestOrigin = origin
+                        mGeoLocationCallback = callback
+                        ActivityCompat.requestPermissions(
+                            this@MainActivity,
+                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                            MY_PERMISSIONS_REQUEST_LOCATION
+                        )
+                    }
+                } else {
+                    callback?.invoke(origin, true, false)
+                }
+            }
+        }
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.settings.allowContentAccess = true
+        myWebView.settings.domStorageEnabled = true
+        myWebView.settings.useWideViewPort = true
+        myWebView.loadUrl("your url")
+    }
+
+    //You can deal with the denial of access to the location the way you want in this case I used a Toast
+    private fun handlerUserLocationPermissionDenial() {
+        Toast.makeText(
+            this,
+            "Permission to use location denied",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mGeoLocationCallback?.invoke(mGeoLocationRequestOrigin, true, false)
+                } else {
+                    mGeoLocationCallback?.invoke(mGeoLocationRequestOrigin, false, false)
+                }
+            }
+        }
+    }
+
+}
